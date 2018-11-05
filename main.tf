fe589f1aa88f51d7393db154c92470f0b0ecf11d
chris-malloy <chrismalloymusic@gmail.com>
2018-11-05 16:41:49 -0500

    adding datadog module

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ module "fluentd" {
   ecs_cluster          = "${aws_ecs_cluster.base_cluster.name}"
   fluentd_docker_image = "${var.fluentd_docker_image}"
 }
+# Comment this out if you're not using datadog
+module "datadog" {
+  source = "./logging/datadog"
+
+  env = "${var.env}"
+  dd_agent_api_key = "${var.dd_agent_api_key}"
+  ecs_cluster = "${aws_ecs_cluster.base_cluster.name}"
+}
