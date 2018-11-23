029b2d35a941cb85e6d65103ad8aa4e61f9e2ea5
jduv <jduv@7factor.io>
2018-11-22 21:04:56 -0500

    Updating ECS cluster to use a launch template instead of configuration

--- asg.tf
+++ asg.tf
@@ -17,14 +17,13 @@ data "aws_ami" "aws_linux_ecs" {
   }
 }
 
-resource "aws_launch_configuration" "ecs_container_instance" {
-  name                 = "ecs-launch-configuration"
-  instance_type        = "${var.instance_type}"
-  key_name             = "${var.key_name}"
-  image_id             = "${data.aws_ami.aws_linux_ecs.id}"
-  iam_instance_profile = "${aws_iam_instance_profile.ecs_for_ec2_profile.id}"
+resource "aws_launch_template" "ecs_container_template" {
+  name          = "${var.env}-ecs-asg"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+  image_id      = "${data.aws_ami.aws_linux_ecs.id}"
 
-  security_groups = [
+  vpc_security_group_ids = [
     "${aws_security_group.ecs_boxes.id}",
     "${var.utility_accessible_sg}",
   ]
@@ -34,10 +33,18 @@ resource "aws_launch_configuration" "ecs_container_instance" {
 sudo /bin/su -c 'echo ECS_CLUSTER=${aws_ecs_cluster.base_cluster.name} >> /etc/ecs/ecs.config'
 sudo /bin/su -c 'echo ECS_AVAILABLE_LOGGING_DRIVERS="${var.ecs_logging}" >> /etc/ecs/ecs.config'
 EOF
+
+  iam_instance_profile {
+    name = "${aws_iam_instance_profile.ecs_for_ec2_profile.id}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "ecs_asg" {
-  name = "${var.env}-ecs-asg"
+  name = "${var.env}-ecs-asg-tmpl${aws_launch_template.ecs_container_template.latest_version}"
 
   health_check_grace_period = "${var.health_check_grace_period}"
   health_check_type         = "EC2"
@@ -45,7 +52,11 @@ resource "aws_autoscaling_group" "ecs_asg" {
   min_size                  = "${var.min_size}"
   max_size                  = "${var.max_size}"
   vpc_zone_identifier       = ["${var.asg_subnets}"]
-  launch_configuration      = "${aws_launch_configuration.ecs_container_instance.name}"
+
+  launch_template = {
+    id      = "${aws_launch_template.ecs_container_template.id}"
+    version = "$$Latest"
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -53,13 +64,13 @@ resource "aws_autoscaling_group" "ecs_asg" {
 
   tag {
     key                 = "Name"
-    value               = "ECSInstance"
+    value               = "ECS Instance"
     propagate_at_launch = true
   }
 
   tag {
     key                 = "Cluster"
-    value               = "BaseECSInstances"
+    value               = "Base ECS Instances"
     propagate_at_launch = true
   }
 
