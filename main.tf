0b79bafb807f3804099e33dfe5af971bd27b3891
chris-malloy <chrismalloymusic@gmail.com>
2019-01-04 14:41:25 -0500

    pulling out ami terraform to main file

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=0.10.7"
+}
+
+data "aws_ami" "aws_linux_ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
