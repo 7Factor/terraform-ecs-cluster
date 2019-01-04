e88ace58b21b5e8c7e0646cf95b3535278a65f80
chris-malloy <chrismalloymusic@gmail.com>
2019-01-04 14:38:38 -0500

    adding autoscaling policies and cloudwatch alarms

---
+++ cw.tf
@@ -0,0 +1,71 @@
+# A CloudWatch alarm that monitors CPU utilization of cluster instances for scaling up
+resource "aws_cloudwatch_metric_alarm" "ecs_asg_instances_cpu_high" {
+  alarm_name          = "ecs-${var.env}-instances-CPU-Utilization-Above-80"
+  alarm_description   = "This alarm monitors ECS ${var.env} instances' CPU utilization for scaling up."
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_actions       = ["${aws_autoscaling_policy.scale_up.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs_asg.name}"
+  }
+}
+
+# A CloudWatch alarm that monitors CPU utilization of cluster instances for scaling down
+resource "aws_cloudwatch_metric_alarm" "ecs_asg_instances_cpu_low" {
+  alarm_name          = "ecs-${var.env}-instances-CPU-Utilization-Below-5"
+  alarm_description   = "This alarm monitors ECS ${var.env} instances' CPU utilization for scaling down."
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "5"
+  alarm_actions       = ["${aws_autoscaling_policy.scale_down.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs_asg.name}"
+  }
+}
+
+# A CloudWatch alarm that monitors memory utilization of cluster instances for scaling up
+resource "aws_cloudwatch_metric_alarm" "ecs_asg_instances_memory_high" {
+  alarm_name          = "ecs-${var.env}-instances-Memory-Utilization-Above-80"
+  alarm_description   = "This alarm monitors ECS ${var.env} instances' memory utilization for scaling up."
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_actions       = ["${aws_autoscaling_policy.scale_down.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs_asg.name}"
+  }
+}
+
+# A CloudWatch alarm that monitors memory utilization of cluster instances for scaling down
+resource "aws_cloudwatch_metric_alarm" "ecs_asg_instances_memory_low" {
+  alarm_name          = "ecs-${var.env}-instances-Memory-Utilization-Below-5"
+  alarm_description   = "This alarm monitors ECS ${var.env} instances' memory utilization for scaling down."
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "5"
+  alarm_actions       = ["${aws_autoscaling_policy.scale_down.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.ecs_asg.name}"
+  }
+}
