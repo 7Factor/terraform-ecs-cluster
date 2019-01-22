e960c96951cb6dd3e519c58dcdd4f34d3ff1c99f
jduv <jduv@7factor.io>
2019-01-22 10:43:15 -0500

    Removing EBS optimized

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ecs_instance_role_id" {
   value       = "${aws_iam_role.ecs_role.id}"
   description = "The ID for the role that grants ECS containers AWS permissions."
 }
+
+output "asg" {
+  value       = "${aws_autoscaling_group.ecs_asg.id}"
+  description = "The ID of the ASG that controls the cluster. Use this to add your own scaling policies ad-hoc."
+}
