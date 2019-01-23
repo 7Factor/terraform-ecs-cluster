9931266655febd61197dfb9355e8c33b45f63b43
jduv <jduv@7factor.io>
2019-01-23 14:26:06 -0500

    Cleaning up a few items

--- sg.tf
+++ sg.tf
@@ -31,15 +31,8 @@ resource "aws_security_group" "ecs_web_lb" {
 
 resource "aws_security_group" "ecs_boxes" {
   name        = "ecs-tasks-sg-${var.region}"
-  description = "Allow inbound access from the Web ALB only."
+  description = "Allow inbound access from a set of ALBs based on an assigned security group."
   vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = ["${aws_security_group.ecs_web_lb.id}"]
-  }
 
   egress {
     from_port   = 0
@@ -51,4 +44,15 @@ resource "aws_security_group" "ecs_boxes" {
   tags {
     Name = "ECS Cluster Instances"
   }
+}
+
+resource "aws_security_group_rule" "allow_lb_access_rule" {
+    from_port       = 0
+  type = "ingress"
+    to_port         = 0
+    protocol        = "-1"
+    security_group_id = "${aws_security_group.ecs_boxes}"
+    security_groups = ["${aws_security_group.ecs_web_lb.id}"]
+
+  depends_on = ["aws_security_group.ecs_boxes", "aws_security_group.ecs_web_lb"]
 }
