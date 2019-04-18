8069eec32145adba8d0e400771962aa7aed306cf
chris-malloy <chrismalloymusic@gmail.com>
2019-04-18 15:31:47 -0400

    Adding cloudwatch access to ecs instance profile.

--- iam.tf
+++ iam.tf
@@ -28,3 +28,33 @@ resource "aws_iam_role_policy_attachment" "ecs_role_policy_attachment" {
   role       = "${aws_iam_role.ecs_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
+# gives ecs container instances the ability to create cloudwatch assets as needed
+resource "aws_iam_policy" "cloudwatch_access_policy" {
+  name = "cloudwatch-access-policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_access_policy_attachment" {
+  role       = "${aws_iam_role.ecs_role.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch_access_policy.arn}"
+}
