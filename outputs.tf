78e0ebf4d20fedc611ec1ff2f82ca059ec56bbfd
chris-malloy <chrismalloymusic@gmail.com>
2019-05-04 19:30:49 -0400

    Adding asg config to outputs for quality of life.

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,16 @@ output "asg" {
   description = "The ID of the ASG that controls the cluster. Use this to add your own scaling policies ad-hoc."
 }
 
+output "asg_minimum_capacity" {
+  value       = "${aws_autoscaling_group.ecs_asg.min_size}"
+  description = "The minumum capacity of the ASG."
+}
+
+output "asg_maximum_capacity" {
+  value       = "${aws_autoscaling_group.ecs_asg.max_size}"
+  description = "The maximum capacity of the ASG."
+}
+
 output "ecs_lb_sg_id" {
   value       = "${aws_security_group.ecs_accessible_sg.id}"
   description = "The ID of the security group to attach to load balancers for health checks."
