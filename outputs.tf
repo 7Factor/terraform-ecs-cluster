44537cfa95e3521c0c93487660c87848f9248d40
chris-malloy <chrismalloymusic@gmail.com>
2020-06-03 22:39:34 -0400

    Testing cluster.

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "ecs_instance_role_name" {
   description = "The ID for the role that grants ECS containers AWS permissions."
 }
 
-output "asg" {
+output "asg_id" {
   value       = aws_autoscaling_group.ecs_asg.id
   description = "The ID of the ASG that controls the cluster. Use this to add your own scaling policies ad-hoc."
 }
