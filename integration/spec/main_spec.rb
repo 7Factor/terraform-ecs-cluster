fc8b252fc9f318efcb1495af2bb910aa6b80c5a3
chris-malloy <chrismalloymusic@gmail.com>
2020-06-05 14:29:22 -0400

    Testing container instances is too brittle as awspec will not wait for instance to be initialized.

--- integration/spec/main_spec.rb
+++ integration/spec/main_spec.rb
@@ -8,7 +8,6 @@ TFOUTPUTS = eval(ENV['KITCHEN_KITCHEN_TERRAFORM_OUTPUTS'])
 describe ecs_cluster(TFVARS['ecs_cluster_name']) do
   it { should exist }
   it { should be_active }
-  its(:registered_container_instances_count) { should eq 1}
 end
 
 describe autoscaling_group(TFOUTPUTS[:asg_id][:value]) do
