e82e07c33c9273132547f8bff57510a4e124c164
Jeremy Duvall <jduv@7factor.io>
2021-03-23 17:03:29 -0400

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_cluster" "the_cluster" {
 }
 
 resource "aws_ecs_capacity_provider" "primary" {
-  name = aws_autoscaling_group.this.name
+  name = aws_autoscaling_group.ecs_asg.name
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs_asg.arn
